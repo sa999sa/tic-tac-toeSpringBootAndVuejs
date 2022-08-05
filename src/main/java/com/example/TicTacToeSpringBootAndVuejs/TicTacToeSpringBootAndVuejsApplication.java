cb1afd2da36e4689357ab398b5bde27127ba2b9c
sa999na <lumimosita999@icloud.com>
2022-08-05 22:38:17 +0900

    プロジェクト作成

---
+++ src/main/java/com/example/TicTacToeSpringBootAndVuejs/TicTacToeSpringBootAndVuejsApplication.java
@@ -0,0 +1,13 @@
+package com.example.TicTacToeSpringBootAndVuejs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeSpringBootAndVuejsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeSpringBootAndVuejsApplication.class, args);
+	}
+
+}
